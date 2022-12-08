b6dc5f74d2b2b9a93f962d4c2578f7813884725b
ARKINUX <88921403+sudoark@users.noreply.github.com>
2022-12-08 16:03:10 +0530

    Add files via upload

---
+++ js/responsive.js
@@ -0,0 +1,8 @@
+/*burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-class-resp');
+    navList.classList.toggle('v-class-resp');
+})*/
